cf545cc9b631c672d89bad16dce5eeb33c2a95e1
ThomasPieter2 <32322879+PieterThomas@users.noreply.github.com>
2018-12-01 13:55:14 +0100

    Mobile first begonn

--- script/app.js
+++ script/app.js
@@ -2,18 +2,18 @@
 
 // API: https://pokeapi.co/api/v2/pokemon/
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     getAPI();
 });
 
 let getAPI = () => {
     var url = "https://pokeapi.co/api/v2/pokemon/";
 
-    fetch(url).then(function(response) {
+    fetch(url).then(function (response) {
             //console.log(response);
             return response.json();
         })
-        .then(function(json) {
+        .then(function (json) {
             console.log(json);
             var data = json.results;
             showResult(data);
@@ -30,21 +30,19 @@ let showResult = (data) => {
 };
 
 let createDiv = (pokelist) => {
-    var divMain, btnPokemon, newDivFull, divPokemon, imgPokemon, pPokemon, url;
+    var divMain, btnPokemon, divPokemon, imgPokemon, pPokemon, url;
 
-    for (var i = 0; i < pokelist.length; i++) {
+    // for (var i = 0; i < pokelist.length; i++) {
+    for (var i = 0; i < 30; i++) {
         divMain = document.createElement('div');
-        divMain.className = 'c-pokemon-div';
+        divMain.className = 'o-layout__item o-layout__gutter u-1-of-3-bp1 u-1-of-3-bp2 u-1-of-3-bp3 c-pokemon';
 
         btnPokemon = document.createElement('button');
         btnPokemon.id = pokelist[i].name;
-        btnPokemon.addEventListener('click', function() {
+        btnPokemon.addEventListener('click', function () {
             showInfoPokemon(this.id);
         });
         btnPokemon.className = 'o-button-reset c-button-pokemon';
-
-        newDivFull = document.createElement('div');
-        newDivFull.className = 'c-pokemon';
 
         imgPokemon = document.createElement('img');
         url = "http://static.pokemonpets.com/images/monsters-images-800-800/" + (i + 1) + "-" + pokelist[i].name + ".png";
@@ -55,21 +53,18 @@ let createDiv = (pokelist) => {
         pPokemon = document.createElement('p');
         pPokemon.innerHTML = pokelist[i].name.charAt(0).toUpperCase() + pokelist[i].name.slice(1);
 
-        divPokemon = document.createElement('div');
-        divPokemon.className = "c-pokemon__info";
-        divPokemon.appendChild(imgPokemon);
-        divPokemon.appendChild(pPokemon);
+        btnPokemon.appendChild(imgPokemon);
+        btnPokemon.appendChild(pPokemon);
 
-        newDivFull.appendChild(divPokemon);
-        btnPokemon.appendChild(newDivFull);
         divMain.appendChild(btnPokemon);
+
         document.getElementById('main').appendChild(divMain);
     }
 }
 
 let showInfoPokemon = (pokelist) => {
     var div = document.getElementById('info');
-    document.addEventListener('click', function(e) {
+    document.addEventListener('click', function (e) {
         console.log(e.target.getAttribute('id'), div.getAttribute('id'));
         console.log(pokelist);
         if ((e.target.getAttribute('id') != div.getAttribute('id')) && (e.target.getAttribute('id') != 'null')) {
