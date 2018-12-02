016bc792d11c5cdc3a5eb53f665ed0075ff86e89
ThomasPieter2 <32322879+PieterThomas@users.noreply.github.com>
2018-12-02 18:54:45 +0100

    Micro interaction

--- script/app.js
+++ script/app.js
@@ -1,10 +1,22 @@
 "use strict"
 
-// API: https://pokeapi.co/api/v2/pokemon/
-
 document.addEventListener('DOMContentLoaded', function () {
     getAPI();
 });
+
+let getData = (id) => {
+    var url = "https://thppokedex.azurewebsites.net/api/v1/pokemon/";
+    fetch(url).then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            var sorted = json.sort((a, b) => {
+                return a.id - b.id;
+            });
+            var pokemon = sorted[id];
+            vulGegevensIn(pokemon, getInfo(sorted));
+        });
+}
 
 let getAPI = () => {
     var url = "https://thppokedex.azurewebsites.net/api/v1/pokemon/";
@@ -21,24 +33,23 @@ let getAPI = () => {
 }
 
 let createDiv = (pokelist) => {
-    var divMain, btnPokemon, imgPokemon, pPokemon, url;
+    var divMain, btnPokemon, imgPokemon, pPokemon;
 
-    // for (var i = 0; i < pokelist.length; i++) {
+    //for (var i = 0; i < pokelist.length; i++) {
     for (var i = 0; i < 30; i++) {
         divMain = document.createElement('div');
         divMain.id = pokelist[i].name;
         divMain.className = 'o-layout__item o-layout__gutter u-1-of-3-bp1 u-1-of-3-bp2 u-1-of-3-bp3 c-pokemon';
 
         btnPokemon = document.createElement('button');
-        btnPokemon.id = i //pokelist[i].name;
+        btnPokemon.id = i
         btnPokemon.addEventListener('click', function () {
             showInfoPokemon(this.id);
         });
         btnPokemon.className = 'o-button-reset c-button-pokemon';
 
         imgPokemon = document.createElement('img');
-        url = "http://static.pokemonpets.com/images/monsters-images-800-800/" + pokelist[i].id + "-" + pokelist[i].name + ".png";
-        imgPokemon.src = url;
+        imgPokemon.src = pokelist[i].url;
         imgPokemon.className = 'c-pokemon_img';
         imgPokemon.alt = pokelist[i].name;
 
@@ -65,8 +76,7 @@ let showInfoPokemon = (pokemon) => {
             div.style.visibility = "hidden";
         }
     });
-
-
+    // Met escape knop het info venster sluiten
     // document.addEventListener("keyup", function (e) {
     //     if (e.key == "Escape") {
     //         div.style.zIndex = -1;
@@ -76,28 +86,58 @@ let showInfoPokemon = (pokemon) => {
 }
 
 
-let vulGegevensIn = (pokemon) => {
+let vulGegevensIn = (pokemon, info) => {
     document.getElementById("txtName").value = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
     document.getElementById("txtId").value = pokemon.id;
-    document.getElementById("txtHeight").value = pokemon.height;
-    document.getElementById("txtWeight").value = pokemon.weight;
+
     imgPokemon = document.getElementById("imgPokemon")
     imgPokemon.src = pokemon.url;
-    imgPokemon.className = 'c-pokemon_img';
+    imgPokemon.className = 'c-pokemon_img--div';
     imgPokemon.alt = pokemon.name;
     console.log("Gegevens ingevuld");
+
+    var maxH = info[0],
+        maxW = info[1];
+
+    animationHeight(pokemon, maxH, "height");
+    animationHeight(pokemon, maxW, "weight");
 }
 
-let getData = (id) => {
-    var url = "https://thppokedex.azurewebsites.net/api/v1/pokemon/";
-    fetch(url).then(function (response) {
-            return response.json();
-        })
-        .then(function (json) {
-            var sorted = json.sort((a, b) => {
-                return a.id - b.id;
-            });
-            var pokemon = sorted[id];
-            vulGegevensIn(pokemon);
-        });
+let getInfo = (pokemon) => {
+    var maxH, maxW;
+    var arrayHeight = [],
+        arrayWeight = [];
+    for (var i = 0; i < pokemon.length; i++) {
+        arrayHeight.push(pokemon[i].height);
+        arrayWeight.push(pokemon[i].weight);
+    }
+    maxH = Math.max(...arrayHeight);
+    maxW = Math.max(...arrayWeight);
+
+    return [maxH, maxW];
+}
+
+let animationHeight = (pokemon, max, type) => {
+    var elem = document.getElementById("isLoading" + type);
+    elem.style.width = '0%';
+
+    var width = 1;
+    var id = setInterval(frame, 10);
+    var maxPokemon;
+    if (type == "height") {
+        elem.innerHTML = pokemon.height;
+        maxPokemon = (pokemon.height / max) * 100;
+    } else {
+        elem.innerHTML = pokemon.weight;
+        maxPokemon = (pokemon.weight / max) * 100;
+    }
+
+    function frame() {
+        if (width >= maxPokemon) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+        }
+    }
 }
